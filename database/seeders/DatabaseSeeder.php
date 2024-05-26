7cf318e51733d67153904e4a19dcbf0bc085c458
wei <wendyhsu09@gmail.com>
2024-05-26 21:47:20 +0800

    add:create project and seeding data

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use I lluminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory(1000)->create();
+
+        $this->call(EventSeeder::class);
+        $this->call(AttendeeSeeder::class);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
