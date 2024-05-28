3de75f276a60a50b5ecca7e2d36861b3a5ebf912
wei <wendyhsu09@gmail.com>
2024-05-28 13:56:41 +0800

    add:storing data and validate,updating and deleting date

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Event;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -13,6 +14,7 @@ class EventController extends Controller
     public function index()
     {
         //
+        return Event::all();
     }
 
     /**
@@ -20,30 +22,66 @@ class EventController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'start_time' => 'required|date',
+            'end_time' => 'required|date|after:start_time'
+        ]);
+
+        $event = Event::create(array_merge($validatedData, ['user_id' => 1]));
+
+        return $event;
+        // $event = Event::create([
+        //     // 數據做驗證
+        //     'user_id' => 1,
+        //     $request->validate([
+        //         'name' => 'required|string|max:255',
+        //         'description' => 'nullable|string',
+        //         'start_time' => 'required|date',
+        //         'end_time' => 'required|date|after:start_time'
+        //     ])
+
+        // ]);
+
+        // return $event;
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Event $event)
     {
-        //
+        // 
+        return $event;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Event $event)
     {
         //
+        $event->update(
+            $request->validate([
+                'name' => 'sometimes|string|max:255',
+                'description' => 'nullable|string',
+                'start_time' => 'sometimes|date',
+                'end_time' => 'sometimes|date|after:start_time'
+            ])
+        );
+
+        return $event;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Event $event)
     {
         //
+        $event->delete();
+        return response(status: 204);
     }
 }
